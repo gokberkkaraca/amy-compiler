457380ddc4cff508b8a1c0fe33ffbe0d7ef67379
Gokberk Karaca <gskaraca1905@gmail.com>
2017-10-21 18:07:49 +0200

    Partially implement LL1 grammer

--- src/amyc/parsing/Parser.scala
+++ src/amyc/parsing/Parser.scala
@@ -58,13 +58,68 @@ object Parser extends Pipeline[Stream[Token], Program] {
   // You can start from the example and work your way from there.
   // Make sure you use the warning that tells you which part is not in LL1
   val amyGrammarLL1 = Grammar('Program, List[Rules[Token]](
-    ???
+      'Program ::= 'ModuleDefs,
+      'ModuleDefs ::= 'ModuleDef ~ 'ModuleDefs | epsilon(),
+      'ModuleDef ::= OBJECT() ~ 'Id ~ EXTENDS() ~ APP() ~ LBRACE() ~ 'Definitions ~ 'OptExpr ~ RBRACE() ~ EOF(),
+      'Definitions ::= 'Definition ~ 'Definitions | epsilon(),
+      'Definition ::= 'AbstractClassDef | 'CaseClassDef | 'FunDef,
+      'AbstractClassDef ::= ABSTRACT() ~ CLASS() ~ 'Id,
+      'CaseClassDef ::= CASE() ~ CLASS() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ EXTENDS() ~ 'Id,
+      'FunDef ::= DEF() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ COLON() ~ 'Type ~ EQSIGN() ~ LBRACE() ~ 'Expr ~ RBRACE(),
+      'Params ::= epsilon() | 'Param ~ 'ParamList,
+      'ParamList ::= epsilon() | COMMA() ~ 'Param ~ 'ParamList,
+      'Param ::= 'Id ~ COLON() ~ 'Type,
+      'OptExpr ::= 'Expr | epsilon(),
+      'Type ::= INT() | STRING() | BOOLEAN() | UNIT() | 'QName,
+      'QName ::= 'Id ~ 'QNameSeq,
+      'QNameSeq ::= epsilon() | DOT() ~ 'Id,
+
+      'Expr ::= 'UnaryExpr ~ 'ExprSeq,
+
+      'ExprSeq ::= 'TimesDivModExpr ~ 'ExprSeq2,
+      'ExprSeq2 ::= 'PlusMinusConcatExpr ~ 'ExprSeq3,
+      'ExprSeq3 ::= 'LessThanLessEqualsExpr ~ 'ExprSeq4,
+      'ExprSeq4 ::= 'AndExpr ~ 'ExprSeq5,
+      'ExprSeq5 ::= 'OrExpr | epsilon(),
+
+
+      'UnaryExpr ::= BANG() ~ 'Factor | MINUS() ~ 'Factor,
+
+      'TimesDivModExpr ::= 'Factor ~ 'TimesDivModExprSeq,
+      'TimesDivModExprSeq ::= TIMES() ~ 'TimesDivModExpr | DIV() ~ 'TimesDivModExpr | epsilon(),
+
+      'PlusMinusConcatExpr ::= 'Factor ~ 'PlusMinusConcatExprSeq,
+      'PlusMinusConcatExprSeq ::= PLUS() ~ 'PlusMinusConcatExpr | MINUS()  ~ 'PlusMinusConcatExpr | CONCAT() ~ 'PlusMinusConcatExpr,
+
+      'LessThanLessEqualsExpr ::= 'Factor ~ 'LessThanLessEqualsExprSeq,
+      'LessThanLessEqualsExprSeq ::= LESSTHAN() ~ 'LessThanLessEqualsExpr | LESSEQUALS() ~ 'LessThanLessEqualsExpr | epsilon(),
+
+      'AndExpr ::= 'Factor ~ 'AndExprSeq,
+      'AndExprSeq ::= AND() ~ 'AndExpr | epsilon(),
+
+      'OrExpr ::= 'Factor ~ 'OrExprSeq,
+      'OrExprSeq ::= OR() ~ 'OrExpr | epsilon(),
+
+      'Literal ::= TRUE() | FALSE() | LPAREN() ~ RPAREN() | INTLITSENT | STRINGLITSENT,
+      'BinOp ::= PLUS() | MINUS() | TIMES() | DIV() | MOD() | LESSTHAN() | LESSEQUALS() |
+        AND() | OR() | EQUALS() | CONCAT(),
+      'Cases ::= 'Case ~ 'CasesSeq,
+      'CasesSeq ::= 'Cases | epsilon(),
+      'Case ::= CASE() ~ 'Pattern ~ RARROW() ~ 'Expr,
+      'Pattern ::= UNDERSCORE() | 'Literal | 'Id ~ 'PatternSeq,
+      'PatternSeq ::= 'QNameSeq ~ LPAREN() ~ 'Patterns ~ RPAREN() | epsilon(),
+      'Patterns ::= epsilon() | 'Pattern ~ 'PatternList,
+      'PatternList ::= epsilon() | COMMA() ~ 'Pattern ~ 'PatternList,
+      'Args ::= epsilon() | 'Expr ~ 'ExprList,
+      'ExprList ::= epsilon() | COMMA() ~ 'Expr ~ 'ExprList,
+      'Id ::= IDSENT,
+      'Factor ::= 'Id | 'Literal
   ))
 
   def run(ctx: Context)(tokens: Stream[Token]): Program = {
     // TODO: Switch to LL1 when done
-    //val (grammar, constructor) = (amyGrammarLL1, new ASTConstructorLL1)
-    val (grammar, constructor) = (amyGrammar, new ASTConstructor)
+    val (grammar, constructor) = (amyGrammarLL1, new ASTConstructorLL1)
+    //val (grammar, constructor) = (amyGrammar, new ASTConstructor)
 
     import ctx.reporter._
     implicit val gc = new GlobalContext()
